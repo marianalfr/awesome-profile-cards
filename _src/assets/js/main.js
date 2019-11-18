05e522235ad79472eda90fcec8d46ee777fbff1a
marianalfr <marianalfr@gmail.com>
2019-11-18 21:05:15 +0100

    add dynamic started

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -43,39 +43,41 @@ const linkTelf = document.querySelector('#linkTelf');
 
 
 function fillTelf() {
-    if (inputTelf.value !== null){
+    if (inputTelf.value.length !== 0){
         cardTelf.classList.remove('hidden');
         linkTelf.href= `tel:${inputTelf.value}`;
     }
-    else if (inputTelf.value === null){
+    else {
         cardTelf.classList.add('hidden');
     }
 };
 
-
 function fillEmail() {
-    if (inputEmail.value !== null){
+    if (inputEmail.value.length !== 0){
         cardEmail.classList.remove('hidden');
         linkEmail.href = `mailto:${inputEmail.value}`;
+    }
+    else {
+        cardLEmail.classList.add('hidden');
     }
 };
 
 function fillLinkedin() {
-    if (inputLinkedin.value !== null){
+    if (inputLinkedin.value.length !== 0){
         cardLinkedin.classList.remove('hidden');
         linkLinkedin.href= `https://www.linkedin.com/in/${inputLinkedin.value}/`;
     }
-    else if (inputLinkedin.value === null){
+    else {
         cardLinkedin.classList.add('hidden');
     }
 };
 
 function fillGithub() {
-    if (inputGithub.value !== null){
+    if (inputGithub.value.length !== 0){
         cardGithub.classList.remove('hidden');
         linkGithub.href= `https://www.github.com/${inputGithub.value}`;
     }
-    else if (inputGithub.value === null){
+    else {
         cardGithub.classList.add('hidden');
     }
 };
@@ -84,3 +86,34 @@ inputTelf.addEventListener('change', fillTelf);
 inputEmail.addEventListener('change', fillEmail);
 inputLinkedin.addEventListener('change', fillLinkedin);
 inputGithub.addEventListener('change', fillGithub);
+
+/////////////////////////////////////////////////////////
+// Add color preview
+
+const palette1 = document.querySelector('#palette1');
+const palette2 = document.querySelector('#palette2');
+const palette3 = document.querySelector('#palette3');
+const cardPreview = document.querySelector('#cardId');
+
+
+
+    function previewPalette1 () {
+        cardPreview.classList.add(select-palette1);
+        cardPreview.classList.remove(select-palette2);
+        cardPreview.classList.remove(select-palette3);
+    };
+     function previewPalette2() {
+        cardPreview.classList.add(select-palette2);
+        cardPreview.classList.remove(select-palette1);
+        cardPreview.classList.remove(select-palette3);
+    };
+    function previewPalette3() {
+        cardPreview.classList.add(select-palette3);
+        cardPreview.classList.remove(select-palette2);
+        cardPreview.classList.remove(select-palette1);
+    };
+
+palette1.addEventListener ('change',previewPalette1);
+palette2.addEventListener ('change',previewPalette2);
+palette3.addEventListener ('change',previewPalette3);
+
