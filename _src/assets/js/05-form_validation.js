4d5d0e152ade0e3b8fd3a50c3f89c6d95a3b6927
Ana Arribas <arribas.ibd@gmail.com>
2019-11-23 17:32:57 +0100

    Trying to add mobile validation

--- _src/assets/js/05-form_validation.js
+++ _src/assets/js/05-form_validation.js
@@ -2,16 +2,31 @@
 
 //// Individual validation
 
-function validateEmail(){
+function validateEmail() {
     const emailError = document.querySelector('#error-email');
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputEmail.value)) {
+        inputEmail.classList.remove('input-error');
         emailError.classList.add('hidden');
         return (true);
-      } else {
+    } else {
         inputEmail.classList.add('input-error');
         emailError.classList.remove('hidden');
         return (false);
     }
+}
+
+function validateTelf() {
+    const telfError = document.querySelector('#error-telf');
+    if (/^[\s\S]{0,9}$/.test(inputTelf.value)) {
+        inputTelf.classList.remove('input-error');
+        telfError.classList.add('hidden');
+        return (true)
+    } else {
+        inputTelf.classList.add('input-error');
+        telfError.classList.remove('hidden');
+        return (false);
+    }
+
 }
 
 inputEmail.addEventListener('change', validateEmail);
@@ -24,8 +39,8 @@ inputTelf.addEventListener('change', validateTelf);
 const cardForm = document.querySelector('#card__form');
 const buttonCreate = document.querySelector('#btn-create');
 
-function inactiveButton (){
-    if (cardForm.checkValidity() === true){
+function inactiveButton() {
+    if (cardForm.checkValidity() === true) {
         buttonCreate.classList.remove('btn-create-inactive');
     } else {
         buttonCreate.classList.add('btn-create-inactive');
