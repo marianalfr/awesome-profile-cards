1680340f2c8ea5d9b04b91672e41fb02e9cb2325
marianalfr <marianalfr@gmail.com>
2019-11-23 16:45:23 +0100

    Add image load

--- _src/assets/js/05-form_validation.js
+++ _src/assets/js/05-form_validation.js
@@ -1,5 +1,23 @@
 'use strict';
 
+//// Individual validation
+
+function validateEmail(){
+    const emailError = document.querySelector('#error-email');
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputEmail.value)) {
+        emailError.classList.add('hidden');
+        return (true);
+      } else {
+        inputEmail.classList.add('input-error');
+        emailError.classList.remove('hidden');
+        return (false);
+    }
+}
+
+inputEmail.addEventListener('change', validateEmail);
+inputTelf.addEventListener('change', validateTelf);
+
+
 /////////////////////////////////////////////////////////
 // form validation
 
@@ -15,3 +33,4 @@ function inactiveButton (){
 }
 
 formTopShare.addEventListener('click', inactiveButton);
+
